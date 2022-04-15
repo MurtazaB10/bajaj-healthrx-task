e6c33f64c07dcf0c539e45b13feb4715f4dd944f
AMDesigners <amdesigners52@gmail.com>
2022-04-15 13:34:40 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const makeAPICall = async () => {
+    try {
+      const response = await axios.get(
+        "https://bajaj-healthrx-task.herokuapp.com/"
+      );
+      console.log(response.data);
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    makeAPICall();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map((key, ind) => {
+          return (
+            <>
+              <li>{key.login}</li>
+              <li>{key.avatar_url}</li>
+              <li>{key.url.html_url}</li>
+              <li>{key.type}</li>
+              <hr />
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
